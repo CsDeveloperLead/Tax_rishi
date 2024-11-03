ac73d3c56d38e5657c4517a61c4deb210c8e9ccc
Aftab1311 <md.aftabehsan@gmail.com>
2024-11-03 14:58:58 +0530

    minor change

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,15 @@
 import AboutSection1 from "../components/AboutComponents/AboutSection1";
 import AboutSection2 from "../components/AboutComponents/AboutSection2";
-// import Teams from "../components/AboutComponents/Teams";
-// import TestimonialSection from "../components/HomeComponents/TestimonialSection";
+ import Teams from "../components/AboutComponents/Teams";
+ import TestimonialSection from "../components/HomeComponents/TestimonialSection";
 
 const About = () => {
   return (
     <div>
       <AboutSection1 />
       <AboutSection2 />
-      {/* <Teams />
-      <TestimonialSection /> */}
+       <Teams />
+      <TestimonialSection /> 
     </div>
   )
 }
